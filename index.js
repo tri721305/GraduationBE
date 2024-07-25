977680142907d5c14a90ed870768eee2a41c76a7
193278-DHMTri <dhmt721305@gmail.com>
2024-07-25 22:14:04 +0700

    config Firebase

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
-
+app.use("/api/upload", uploadRoutes);
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
